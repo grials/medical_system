ae0151dbe2adeda5062899d9b7465f75b13bf72e
José Mendez <jose.mendez@pegasi.io>
2024-09-18 22:55:29 -0400

    fix: finish day

--- infrastructure/start.sh
+++ infrastructure/start.sh
@@ -4,8 +4,10 @@ docker volume rm local_mongo-data-vol-one || true
 
 docker compose -f ./infrastructure/compose-kafka.yaml down 
 docker compose -f ./infrastructure/compose-redis.yaml down 
+docker compose -f ./infrastructure/traefik/compose.yaml down 
 docker compose -f ./infrastructure/compose-mongo-db.yaml down 
 
 docker compose -f ./infrastructure/compose-kafka.yaml up -d 
 docker compose -f ./infrastructure/compose-redis.yaml up -d 
+docker compose -f ./infrastructure/traefik/compose.yaml up -d 
 docker compose -f ./infrastructure/compose-mongo-db.yaml up -d 
